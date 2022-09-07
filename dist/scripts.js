c2913255dafdc78625335ed302127f6dcb9c48f2
hidhz <hidhzdev@gmail.com>
2022-09-07 12:36:41 +0700

    first

---
+++ dist/scripts.js
@@ -0,0 +1,19 @@
+function kliklweb(){
+Swal.fire({
+  title: '<strong>Landing <u>Page</u></strong>',
+  icon: 'info',
+  html:
+    `</div class="text-white">
+	<h1>Infooo</h1>
+    </div>`,
+  showCloseButton: true,
+  showCancelButton: true,
+  focusConfirm: false,
+  confirmButtonText:
+    '<i class="fa fa-thumbs-up"></i> pesan jasa',
+  confirmButtonAriaLabel: 'Thumbs up, great!',
+  cancelButtonText:
+    '<i class="fa fa-thumbs-down"></i>',
+  cancelButtonAriaLabel: 'Thumbs down'
+})
+}
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+        colors: {
+            clifford: '#da373d',
+            pink1: '#f472b6',
+            biru: '#00e0ff',
+            abu2: '#c4c4c4',
+            primary: '#0ea5e9',
+            dark: '#111827',
+            hijau: '#128650',
+            abu22: '#303A44',
+            merah: '#CF2F4C',
+            hijaumac: '#2ACE42',
+            kuningmac: '#FFBE30',
+            merahmac: '#FF6159',
+            abumac: '#ECEAEB',
+            ungu: '#CED7FF',
+        },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false
+  },
+}
