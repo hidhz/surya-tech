cebe3e88ab3920fa0f507f648447279bbc9a07ae
hidhz <hidhzdev@gmail.com>
2022-09-08 05:30:32 +0700

    updated

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,19 +1,123 @@
-function kliklweb(){
-Swal.fire({
-  title: '<strong>Landing <u>Page</u></strong>',
-  icon: 'info',
+function kliklwin(){
+ Swal.fire({
   html:
     `</div class="text-white">
-	<h1>Infooo</h1>
+        <h1 class="text-3xl font-bold text-primary">Installasi Windows</h1>
+	<p class="text-white">Windows 7 dan windows 10</p>
+	<p class="pt-2 text-white">Install ulang laptop atau komputer anda menggunakan jasa kami. Aman, terpercaya dan murah.</p>
+	<hr class="border-white mt-4 pb-4">
+	<p class="line-through text-white">Rp. 70.000</p>
+	<h1 class="text-white font-bold text-3xl">Rp. 50.000</h1>
+	<div class="pt-6">
+	  <h1 class="text-primary text-xl font-bold">Keuntungan</h1>
+	  <ul class="list-disc text-white ml-2">
+	    <li>Proses cepat</li>
+	    <li>Garansi 1 bulan</li>
+	    <li>Free Install software
+	      <ul class="list-decimal ml-4">
+		<li>Microsoft Office</li>
+		<li>Corel Draw</li>
+		<li>Adobe pscc 2019</li>
+		<li>zoom</li>
+		<li>dan lain-lain</li>
+	      </ul>
+	    </li>
+	  </ul>
+	</div>
     </div>`,
-  showCloseButton: true,
+  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM_XoFLzkzQnJUCADu8LGbQTn4NkB4e1UTRw&usqp=CAU',
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: 'Custom image',
   showCancelButton: true,
-  focusConfirm: false,
-  confirmButtonText:
-    '<i class="fa fa-thumbs-up"></i> pesan jasa',
-  confirmButtonAriaLabel: 'Thumbs up, great!',
-  cancelButtonText:
-    '<i class="fa fa-thumbs-down"></i>',
-  cancelButtonAriaLabel: 'Thumbs down'
+  cancelButtonText: "cancel",
+  confirmButtonText: "whatsapp",
+}).then((result) => {
+  if(result.isConfirmed){
+    window.location.href = 'https://wa.me/6285859813922'
+  }
 })
 }
+
+function kliklapp(){
+ Swal.fire({
+  html:
+    `</div class="text-white">
+        <h1 class="text-3xl font-bold text-primary">Install & Aktivasi Software</h1>
+        <p class="text-white">#software</p>
+        <p class="pt-2 text-white">Install software pada laptop atau komputer anda menggunakan jasa kami. Aman, terpercaya dan murah.</p>
+        <hr class="border-white mt-4 pb-4">
+        <p class="line-through text-white">Rp. 50.000</p>
+        <h1 class="text-white font-bold text-3xl">Rp. 20.000</h1>
+        <div class="pt-6">
+          <h1 class="text-primary text-xl font-bold">Keuntungan</h1>
+          <ul class="list-disc text-white ml-2">
+            <li>Proses cepat</li>
+            <li>Garansi 1 bulan</li>
+            <li>Daftar software
+              <ul class="list-decimal ml-4">
+                <li>Microsoft Office</li>
+                <li>Corel Draw</li>
+                <li>Adobe pscc 2019</li>
+                <li>zoom</li>
+                <li>dan lain-lain</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+    </div>`,
+  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8BFtucsCgIow6yGkNEudO5URpQJ_VYsaE2A&usqp=CAU',
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: 'Custom image',
+  showCancelButton: true,
+  cancelButtonText: "cancel",
+  confirmButtonText: "whatsapp",
+}).then((result) => {
+  if(result.isConfirmed){
+    window.location.href = 'https://wa.me/6285859813922'
+  }
+})
+}
+
+function kliklweb(){
+ Swal.fire({
+  html:
+    `</div class="text-white">
+        <h1 class="text-3xl font-bold text-primary">Jasa Pembuatan Web</h1>
+        <p class="text-white">Website Landing Page</p>
+        <p class="pt-2 text-white">Buat website anda menggunakan jasa kami. Aman, terpercaya dan murah.</p>
+        <hr class="border-white mt-4 pb-4">
+        <p class="line-through text-white">Rp. 120.000</p>
+        <h1 class="text-white font-bold text-3xl">Rp. 100.000</h1>
+        <div class="pt-6">
+          <h1 class="text-primary text-xl font-bold">Layanan</h1>
+          <ul class="list-disc text-white ml-2">
+            <li>Landing page 1 halaman</li>
+            <li>Free edit image</li>
+	    <li>Proses 3 hari (setelah pembayaran)</li>
+	    <li>Support maintenance</li>
+	    <li>Panduan setup</li>
+            <li>simple web</li>
+          </ul>
+	  <a class="mt-4 px-5 py-1 font-bold text-white bg-primary rounded-md" href="https://hidhz.netlify.app">demo</a>
+        </div>
+    </div>`,
+  imageUrl: 'https://img.freepik.com/free-vector/hand-drawn-web-developers_23-2148819604.jpg?w=740&t=st=1662589262~exp=1662589862~hmac=328620590bf42d357a3581d3fe1360d664d6211e155d38f00dd4f81b6ca98457',
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: 'Custom image',
+  showCancelButton: true,
+  cancelButtonText: "cancel",
+  confirmButtonText: "whatsapp",
+}).then((result) => {
+  if(result.isConfirmed){
+    window.location.href = 'https://wa.me/6285859813922'
+  }
+})
+}
+
+
+
+
+
